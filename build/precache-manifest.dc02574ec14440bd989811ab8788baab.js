7acc6c75f5147287f4e95baaa7ff78761645694e
rohitpal7171 <rohit.pal7171@gmail.com>
2020-01-18 17:13:38 +0530

    'version2'

---
+++ build/precache-manifest.dc02574ec14440bd989811ab8788baab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4fb47445cb70f1735e6ca138672c448",
+    "url": "/Inventory-Management-System/index.html"
+  },
+  {
+    "revision": "7efc831a06635268693f",
+    "url": "/Inventory-Management-System/static/css/main.d5b18751.chunk.css"
+  },
+  {
+    "revision": "22f822d401cba22fafcb",
+    "url": "/Inventory-Management-System/static/js/2.f7e5ace2.chunk.js"
+  },
+  {
+    "revision": "7efc831a06635268693f",
+    "url": "/Inventory-Management-System/static/js/main.540e4607.chunk.js"
+  },
+  {
+    "revision": "9fd19c9685b2ca3bfee4",
+    "url": "/Inventory-Management-System/static/js/runtime~main.5058afa7.js"
+  }
+]);
